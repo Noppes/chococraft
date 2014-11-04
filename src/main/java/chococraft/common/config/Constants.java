80f3c88dfbd0ba0400228e0bf91a21d3968b4468
Clienthax <clienthax@gmail.com>
2014-11-04 16:10:43 +0000

    4.1.3

--- src/main/java/chococraft/common/config/Constants.java
+++ src/main/java/chococraft/common/config/Constants.java
@@ -16,7 +16,7 @@ package chococraft.common.config;
 
 public class Constants
 {
-	public final static String TCC_VERSION = "4.1.2";
+	public final static String TCC_VERSION = "4.1.3";
 	public final static String TCC_MODID = "chococraft";
 	public final static String TCC_NAME = "Clienthax's ChocoCraft";
 	
